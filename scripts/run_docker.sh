c3cd8b8f0f9c6aa45c2f875bc7091430aa9d046c
AmosBunde <git.bunde@hotmail.com>
2024-07-14 10:23:40 +0300

    Edit bash

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
 # Build the docker images
-docker-compose build
+python /app/generate_and_insert_records.py
 
-# Start the services
-docker-compose up -d
+python /app/clickhouse_publisher.py
 
-# Wait for PostgreSQL to be ready
-while ! docker-compose exec postgres pg_isready -U user; do
-  echo "Waiting for PostgreSQL to be ready..."
-  sleep 2
-done
-
-# Run the faker_generate_data.py script to populate the PostgreSQL database
-docker-compose exec app python generate_and_insert_records.py
-
-echo "Docker containers are up and running!"
