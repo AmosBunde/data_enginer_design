b3f19709e08353ee45ccdd8bad248a19fcc1d709
AmosBunde <git.bunde@hotmail.com>
2024-07-14 11:25:58 +0300

    Added wait in bash files

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Wait for PostgreSQL to be ready
+while ! docker-compose exec postgres psql -h postgres -U amos -l &> /dev/null; do
+  sleep 1
+done
+
+# Wait for Kafka to be ready
+while ! docker-compose logs kafka 2>&1 | grep -q 'started'; do
+  sleep 1
+done
+
+# Wait for ClickHouse to be ready
+while ! docker-compose logs clickhouse 2>&1 | grep -q 'started'; do
+  sleep 1
+done
+
 # Build the docker images
 python /app/generate_and_insert_records.py
 
